c87ad9c94d44e7b241f250b9627bff0c81e2584e
Luke Mason <lukjmason@gmail.com>
2021-01-05 16:30:23 -0700

    Make file in src directory for api fetch's

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './scripts.js'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/apple-logo.png';
-import './scripts.js'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+//API methods
+
+// fetch("http://localhost:3001/api/v1/ingredients")
+// .then(response => response.json())
+// .then(data => console.log(data))
+//
+// fetch("http://localhost:3001/api/v1/users")
+// .then(response => response.json())
+// .then(data => console.log(data))
+//
+// fetch("http://localhost:3001/api/v1/recipes")
+// .then(response => response.json())
+// .then(data => console.log(data))
