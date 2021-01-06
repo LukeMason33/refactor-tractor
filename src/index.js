c2d7088625d678681046473d1521baa64c60361b
Luke Mason <lukjmason@gmail.com>
2021-01-05 20:12:55 -0700

    Clean up comments

--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import './scripts.js'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/apple-logo.png';
 
 console.log('This is the JavaScript entry file - your code begins here.');
-
-//API methods
-
-import fetchedData from './fetch.js';
-
-fetchedData.ingredientsAPIData().then(a => console.log(a));
